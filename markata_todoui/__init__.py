b0eb031c7ba1d428bd23cc86b8d78cbfe042cfd2
Waylon Walker <waylon@waylonwalker.com>
2022-05-08 11:12:44 -0500

    Better UI (#5)
    A bunch of fixes to make the UI better and more configurable. Notably posts now cycle, rather thean completely losing the cursor.

--- markata_todoui/__init__.py
+++ markata_todoui/__init__.py
@@ -1,8 +1,9 @@
 import itertools
 import subprocess
-from enum import Enum, auto
+import uuid
+from copy import deepcopy
 from pathlib import Path
-from typing import Optional
+from typing import Dict, Optional
 
 import frontmatter
 from markata import Markata
@@ -13,7 +14,9 @@ from rich.panel import Panel
 from rich.table import Table
 from textual.app import App
 from textual.widget import Widget
-from textual.widgets import Footer, Placeholder
+from textual.widgets import Footer
+
+from markata_todoui.posts import Posts
 
 __version__ = "0.0.1"
 
@@ -33,34 +36,13 @@ DEFAULT_KEYS = {
     "n": "new_post",
     "ctrl+@": "show_config",
     "c": "show_config",
+    "g": "first_post",
+    "G": "last_post",
+    "D": "delete_post",
+    "r": "refresh",
 }
 
-
-class Status(Enum):
-    todo = auto()
-    doing = auto()
-    done = auto()
-
-    def next(self):
-        if self.value == len(self._member_names_):
-            return Status(1)
-        else:
-            return Status(self.value + 1)
-
-    def previous(self):
-        if self.value == 1:
-            return Status(len(self._member_names_))
-        else:
-            return Status(self.value - 1)
-
-
-def nth(iterable, n, default=None):
-    "Returns the nth item or a default value"
-    return next(islice(iterable, n, None), default)
-
-
-def keys_on_file(post):
-    return {key: post[key] for key in frontmatter.load(post.get("path")).keys()}
+GLOBAL_FILTER = "True"
 
 
 class Preview(Widget):
@@ -72,262 +54,143 @@ class Preview(Widget):
         return Panel(Markdown(self.text))
 
 
-class Posts(Widget):
-    def __init__(self, markata: "Markata", title: str, filter: str):
-        super().__init__(title)
-        self.m = markata
-
-        self.title = title
-        self.name = title
-        self.filter = filter
-        self.is_selected = False
-        self.row_selected = 0
-
-    def text(self) -> Optional[str]:
-        for i, post in enumerate(self.m.filter(self.filter)):
-            if i == self.row_selected and self.is_selected:
-                return post.content
-        return None
-
-    def render(self) -> Panel:
-        grid = Table.grid(expand=True)
-
-        for i, post in enumerate(
-            sorted(
-                self.m.filter(self.filter), key=lambda x: x["priority"], reverse=True
-            )
-        ):
-            if i == self.row_selected and self.is_selected:
-                grid.add_row(
-                    f"[red]{post.get('title')}",
-                    f"[bright_black]({post.get('priority')})[/]",
-                    f"[bright_black]{post.get('date')}[/]",
-                )
-            else:
-                grid.add_row(
-                    f"[purple]{post.get('title')}",
-                    f"[bright_black]({post.get('priority')})[/]",
-                    f"[bright_black]{post.get('date')}[/]",
-                )
-        if self.is_selected:
-            self.border_style = "#e8bde3"
-        else:
-            self.border_style = "#c122ac"
-        return Panel(
-            grid,
-            title=f"[#e1af66]{self.title} ({len(self.m.filter(self.filter))})",
-            border_style=self.border_style,
-        )
-
-    def get_current_post(self):
-        for i, post in enumerate(
-            sorted(
-                self.m.filter(self.filter), key=lambda x: x["priority"], reverse=True
-            )
-        ):
-            if i == self.row_selected and self.is_selected:
-                return post
-        return None
-
-    def move_next(self):
-
-        post = self.get_current_post()
-        path = Path(post["path"])
-        post.metadata = keys_on_file(post)
-        post["status"] = Status(Status._member_map_[post["status"]]).next().name
-        path.write_text(frontmatter.dumps(post))
-
-    def raise_priority(self):
-        post = self.get_current_post()
-        path = Path(post["path"])
-        post.metadata = keys_on_file(post)
-        post["priority"] = post["priority"] + 1
-        path.write_text(frontmatter.dumps(post))
-
-    def lower_priority(self):
-        post = self.get_current_post()
-        path = Path(post["path"])
-        post.metadata = keys_on_file(post)
-        post["priority"] = post["priority"] - 1
-        path.write_text(frontmatter.dumps(post))
-
-    def open_post(self):
-
-        post = self.get_current_post()
-        proc = subprocess.Popen(
-            f'nvr --remote "{post["path"]}"',
-            shell=True,
-        )
-        proc.wait()
-
-    def move_previous(self):
-
-        post = self.get_current_post()
-        path = Path(post["path"])
-        post.metadata = keys_on_file(post)
-        post["status"] = Status(Status._member_map_[post["status"]]).previous().name
-        path.write_text(frontmatter.dumps(post))
-
-    def update_markata(self, markata):
-        self.m = markata
-
-    # def render(self):
-    #     return self.__rich__()
-
-    # async def update(self, renderable: RenderableType) -> None:
-    #     self.refresh()
-
-
-class MarkataWidget(Widget):
-    def __init__(self, markata: Markata, widget: str = "server"):
-        super().__init__(widget)
-        self.m = markata
-        self.widget = widget
-        self.renderable = getattr(self.m, self.widget)
-
-    def render(self):
-        return self.renderable
-
-    # async def update(self, renderable: RenderableType) -> None:
-    #     self.renderable = renderable
-    #     self.refresh()
-
-
 class MarkataApp(App):
     async def on_mount(self) -> None:
         self.m.console.quiet = True
         self.preview = Preview(text="init")
-        self.todos = Posts(self.m, "todo", 'status=="todo"')
-        self.doing = Posts(self.m, "doing", 'status=="doing"')
-        self.done = Posts(self.m, "done", 'status=="done"')
+        self.todos = Posts(self.m, "todo", f'{GLOBAL_FILTER} and status=="todo"')
+        self.doing = Posts(self.m, "doing", f'{GLOBAL_FILTER} and status=="doing"')
+        self.done = Posts(self.m, "done", f'{GLOBAL_FILTER} and status=="done"')
         self.stacks = itertools.cycle([self.todos, self.doing, self.done])
-        self.current = self.todos
+        self.current_stack = next(self.stacks)
         self.todos.is_selected = True
+        self.doing.is_selected = False
+        self.done.is_selected = False
         await self.view.dock(
             self.preview, self.todos, self.doing, self.done, edge="left", name="todos"
         )
-        self.set_interval(1, self.action_refresh)
 
     async def on_load(self, event):
         self.m = Markata()
         self.config = self.m.get_plugin_config("todoui")
-        self.config["keys"] = {**self.config.get("keys", {}), **DEFAULT_KEYS}
+        self.config["global_filter"] = GLOBAL_FILTER
 
-        for key, command in self.config.get("keys", {}).items():
+        user_defined_keys = self.config.get("keys", {})
+        self.config["keys"] = {**user_defined_keys, **DEFAULT_KEYS}
+
+        for key, command in self.config.get("keys", None).items():
+
             await self.bind(key, command)
 
     async def action_show_config(self) -> None:
-        self.preview.text = "config\n" + str(self.config)
+        self.preview.text = "config\n" + str(self.config) + self.current_stack.text()
         self.preview.refresh()
 
-    async def action_refresh(self) -> None:
-        # self.refresh()
-        # self.m = Markata()
-        self.m.glob()
-        self.m.load()
-        self.todos.refresh()
-        self.doing.refresh()
-        self.done.refresh()
-        self.preview.text = self.current.text() or ""
+    async def action_refresh(self, update=True, reload=True) -> None:
+        if reload:
+            self.m.glob()
+            self.m.load()
+            self.todos.update(reload=False)
+            self.doing.update(reload=False)
+            self.done.update(reload=False)
+
+        self.preview.text = self.current_stack.text() or ""
         self.preview.refresh()
+        self.refresh()
 
     async def action_next_post(self) -> None:
-        self.current.row_selected += 1
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
+        self.current_stack.next_post()
+        await self.action_refresh(reload=False)
 
     async def action_move_next(self) -> None:
-        self.current.move_next()
+        current_post_uuid = self.current_stack.current_post.get("uuid", "")
+        self.current_stack.move_next()
         self.m.glob()
         self.m.load()
-        self.current.is_selected = False
-        self.current = next(self.stacks)
-        self.current.is_selected = True
-        self.preview.text = self.current.text() or ""
-        # self.action_refresh()
+        await self.action_next_stack()
+        self.current_stack.select_post_by_id(current_post_uuid)
 
     async def action_move_previous(self) -> None:
-        self.current.move_previous()
+        current_post_uuid = self.current_stack.current_post.get("uuid", "")
+        self.current_stack.move_previous()
         self.m.glob()
         self.m.load()
-        self.current.is_selected = False
-        self.current = next(self.stacks)
-        self.current = next(self.stacks)
-        self.current.is_selected = True
-        self.preview.text = self.current.text() or ""
-        # self.action_refresh()
-
-        self.todos.refresh()
-        self.doing.refresh()
-        self.done.refresh()
-        self.preview.refresh()
+        await self.action_prev_stack()
+        self.current_stack.select_post_by_id(current_post_uuid)
 
     async def action_raise_priority(self) -> None:
-        self.current.raise_priority()
+        self.current_stack.raise_priority()
         self.m.glob()
         self.m.load()
-
-        self.current.row_selected -= 1
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
-        # self.action_refresh()
+        self.current_stack.update()
 
     async def action_lower_priority(self) -> None:
-        self.current.lower_priority()
+        self.current_stack.lower_priority()
         self.m.glob()
         self.m.load()
-
-        self.current.row_selected += 1
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
-        # self.action_refresh()
+        self.current_stack.update()
 
     async def action_prev_post(self) -> None:
-        self.current.row_selected -= 1
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
+        self.current_stack.previous_post()
+        await self.action_refresh(reload=False)
 
     async def action_next_stack(self) -> None:
-        self.current.is_selected = False
-        self.current.refresh()
-        self.current = next(self.stacks)
-        self.current.is_selected = True
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
+        self.current_stack.is_selected = False
+        self.current_stack = next(self.stacks)
+        self.current_stack.is_selected = True
+        await self.action_refresh(reload=True)
 
     async def action_prev_stack(self) -> None:
-        self.current.is_selected = False
-        self.current.refresh()
-        self.current = next(self.stacks)
-        self.current = next(self.stacks)
-        self.current.is_selected = True
-        self.current.refresh()
-        self.preview.text = self.current.text() or ""
-        self.preview.refresh()
+        self.current_stack.is_selected = False
+        self.current_stack = next(self.stacks)
+        self.current_stack = next(self.stacks)
+        self.current_stack.is_selected = True
+        await self.action_refresh(reload=True)
 
     async def action_open_post(self) -> None:
-        self.current.open_post()
+        self.current_stack.open_post()
+
+    async def action_first_post(self) -> None:
+        self.current_stack.select_post_by_index(0)
+
+    async def action_last_post(self) -> None:
+        self.current_stack.select_post_by_index(-1)
 
     async def action_new_post(self) -> None:
-        pop_dir = Path(__file__).parents[1]
-        template = "plugins/todo-template"
+        cmd = self.config.get("new_post")
 
         proc = subprocess.Popen(
-            f'tmux popup -d "{pop_dir}" copier copy {template} tasks',
+            cmd,
             shell=True,
         )
         proc.wait()
+        await self.action_refresh(reload=True)
+
+    async def action_delete_post(self) -> None:
+        self.current_stack.delete_current()
 
 
 @hook_impl()
-def cli(app, markata):
+def load(markata: Markata) -> None:
+    for article in markata.articles:
+
+        if "uuid" not in article.keys():
+            article["uuid"] = str(uuid.uuid4())
+            og_keys = frontmatter.loads(Path(article["path"]).read_text()).keys()
+            save_article = deepcopy(article)
+            for key in set(article.keys()) - set(og_keys):
+                if key != "uuid":
+                    print("deleting" + key)
+                    del save_article[key]
+            Path(article["path"]).write_text(frontmatter.dumps(save_article))
+
+
+@hook_impl()
+def cli(app, markata: Markata) -> None:
     @app.command()
-    def todoui():
+    def todoui(global_filter="True") -> None:
+        global GLOBAL_FILTER
+        GLOBAL_FILTER = global_filter
         MarkataApp.run(log="textual.log")
 
 
